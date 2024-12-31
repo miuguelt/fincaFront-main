96acf2bfa871588c9c11f2bd91fde5251b9c8b96
miuguelt <miuguelt@gmail.com>
2024-12-31 01:31:29 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
-import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',  // Permite que la aplicación sea accesible desde otras máquinas
     proxy: {
       '/api': {
-        target: 'https://finca.isladigital.xyz', // URL de la API
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Elimina el prefijo /api
+        target: 'https://finca.isladigital.xyz',   // Dirección de la API (asegúrate de que la URL sea correcta)
+        changeOrigin: true,             // Cambia el origen del host para que la solicitud se vea como si viniera de la API
+        secure: true,                  // Usar true para HTTPS. Si el servidor tiene un certificado válido, debe ser 'true'
+        rewrite: (path) => path.replace(/^\/api/, ''), // Elimina '/api' del path al hacer la solicitud
       },
     },
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+});
+
