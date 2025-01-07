9b88a914275ebf1429202f226f76713459d53adc
miuguelt <miuguelt@gmail.com>
2025-01-06 23:43:09 -0500

    a

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://finca.isladigital.xyz",
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  }
 });
 
 export default api;
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,6 +3,12 @@ import { LoginUser} from '../types/userTypes';
 
 const api = axios.create({
   baseURL: "https://finca.isladigital.xyz",
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  }
 });
 
 
