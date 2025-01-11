25d918ae4dd54ca1882aa3dfd48c6cdd24f35009
miuguelt <miuguelt@gmail.com>
2025-01-10 23:31:18 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,18 +8,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'), // Alias para importaciones
     },
-  },
-  server: {
-    proxy: {
-      '/': {
-        target: 'https://finca.isladigital.xyz',  // URL de tu backend en desarrollo
-        changeOrigin: true,               // Cambia el origen de la solicitud al backend
-        secure: false,                    // Desactiva la verificación SSL (útil para desarrollo)
-        rewrite: (path) => path.replace(/^\//, ''),  // Elimina el prefijo /api
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',  // Carpeta de salida para la construcción
-  },
+  }
 });
